3298148bf752d2bc8c56f695d0685dcea078c9d4
Jacobs Joshua Oyakhire <106456688+jjacobstech@users.noreply.github.com>
2023-12-11 01:25:22 +0100

    Update install.sql

--- install/assets/install.sql
+++ install/assets/install.sql
@@ -19,9 +19,9 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `vms_v3_testing`
+-- Database: `Landy`
 --
-
+CREATE DATABASE `Landy`;
 -- --------------------------------------------------------
 
 --
